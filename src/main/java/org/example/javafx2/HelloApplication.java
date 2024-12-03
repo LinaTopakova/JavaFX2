4617946729addfd5bccd1cb7e581f5b142eca46c
Udynix <129510560+LinaTopakova@users.noreply.github.com>
2024-12-04 03:17:18 +0700

    12

--- src/main/java/org/example/javafx2/HelloApplication.java
+++ src/main/java/org/example/javafx2/HelloApplication.java
@@ -3,21 +3,30 @@ package org.example.javafx2;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage primaryStage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+
+        InputStream icon = getClass().getResourceAsStream("/images/im1.png");
+        assert icon != null;
+        primaryStage.getIcons().add(new Image(icon));
+
+        Scene scene = new Scene(fxmlLoader.load());
+        primaryStage.setTitle("Figure");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         launch();
     }
 }
--- src/main/java/org/example/javafx2/HelloController.java
+++ src/main/java/org/example/javafx2/HelloController.java
@@ -1,14 +1,65 @@
 package org.example.javafx2;
 
+import exceptions.InvalidLength;
+import exceptions.InvalidRadiusValue;
+import geometry2d.Circle;
+import geometry2d.Figure;
+import geometry2d.Rectangle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Canvas canvas = new Canvas();
+    private final List<Figure> figures = new ArrayList<>();
+
+    //private  List<String> colors = Arrays.asList("Красный", "Зеленый", "Голубой", "Желтый", "Черный", "Розовый", "Серый");
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void drawCircle() throws InvalidRadiusValue {
+        Random random = new Random();
+        double x = random.nextDouble() * canvas.getWidth();
+        double y = random.nextDouble() * canvas.getHeight();
+        double radius = 20 + random.nextDouble() * 50;
+        //String color = colors.get(random.nextInt(colors.size()));
+        Color color = Color.color(random.nextDouble(), random.nextDouble(), random.nextDouble());
+
+        //System.out.println(color);
+        figures.add(new Circle(radius, x, y, color));
+        redraw();
+    }
+    @FXML
+    private void drawRectangle() throws InvalidLength {
+        Random random = new Random();
+        double x = random.nextDouble() * canvas.getWidth();
+        double y = random.nextDouble() * canvas.getHeight();
+        double width = 10 + random.nextDouble() * 91;
+        double length = 10 + random.nextDouble() * 91;
+        Color color = Color.color(random.nextDouble(), random.nextDouble(), random.nextDouble());
+        figures.add(new Rectangle(length, width, x,y, color));
+        redraw();
+    }
+    @FXML
+    private void clear() {
+        figures.clear();
+        redraw();
+    }
+
+    private void redraw() {
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        for (Figure figure : figures) {
+            System.out.println("redraw");
+            figure.draw(gc);
+        }
     }
 }
